9d4e91a37c681c5e93d5a250fdb74c0d634b17b2
samuels97 <66149372+samuels97@users.noreply.github.com>
2020-08-14 15:22:49 +0000

    Create index.php
    first page of my dewi app

---
+++ index.php
@@ -0,0 +1,282 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<title>Dewi Building Solutions Limited</title>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="description" content="The River template project">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+
+		<link rel="stylesheet" type="text/css" href="styles/bootstrap-4.1.2/bootstrap.min.css">
+		<link href="plugins/font-awesome-4.7.0/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+		<link rel="stylesheet" type="text/css" href="plugins/OwlCarousel2-2.3.4/owl.carousel.css">
+		<link rel="stylesheet" type="text/css" href="plugins/OwlCarousel2-2.3.4/owl.theme.default.css">
+		<link rel="stylesheet" type="text/css" href="plugins/OwlCarousel2-2.3.4/animate.css">
+		<link href="plugins/jquery-datepicker/jquery-ui.css" rel="stylesheet" type="text/css">
+		<link href="plugins/colorbox/colorbox.css" rel="stylesheet" type="text/css">
+		<link rel="stylesheet" type="text/css" href="styles/main_styles.css">
+		<link rel="stylesheet" type="text/css" href="styles/responsive.css">
+		<link rel="stylesheet" type="text/css" href="css/style.css">
+		<link rel="stylesheet" href="css/bootstrap.min.css">
+	    <link rel="stylesheet" href="css/animate.css">
+	    <link rel="stylesheet" href="css/owl.carousel.min.css">
+	    <link rel="stylesheet" href="css/aos.css">
+	    <link rel="stylesheet" href="css/bootstrap-datepicker.css">
+	    <link rel="stylesheet" href="css/jquery.timepicker.css">
+	    <link rel="stylesheet" href="css/fancybox.min.css">
+	      
+	    <link rel="stylesheet" href="fonts/ionicons/css/ionicons.min.css">
+	    <link rel="stylesheet" href="fonts/fontawesome/css/font-awesome.min.css">
+
+
+	    <link rel="stylesheet" href="css/open-iconic-bootstrap.min.css">
+	    <link rel="stylesheet" href="css/owl.theme.default.min.css">
+	    <link rel="stylesheet" href="css/magnific-popup.css">
+
+	    <link rel="stylesheet" href="css/ionicons.min.css">
+	    <link rel="stylesheet" href="css/flaticon.css">
+	    <link rel="stylesheet" href="css/icomoon.css">
+	</head>
+	<body>
+		<?php include("header.html"); ?>
+
+		<div class="home">
+			<div class="home_slider_container">
+				<div class="owl-carousel owl-theme home_slider">
+					
+					<!-- Slide -->
+					<div class="slide">
+						<div class="background_image" style="background-image:url(images/index_1.jpg)"></div>
+						<div class="home_container">
+							<div class="container">
+								<div class="row">
+									<div class="col">
+										<div class="home_content text-center">
+											<div class="home_title">Dewi Building Solutions</div>
+											
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+
+			<div id="cta">
+		            <div class="col-md-7 text-center" style="margin:20px auto">
+		                <p>The company is self-sufficient in the majority of 
+						construction disciplines placing great emphasis on training its 
+						technicians and craftsmen to produce an exceptionally high standard of 
+						workmanship.<br>
+		                ROKO's main office and extensive operational base, 
+						situated in Kawempe, Kampala District, sources and directs its projects 
+						throughout Uganda and region.<br>
+						Materials are produced locally and from the Preferential
+						 Trade Area wherever possible, and imports are facilitated by ROKO's 
+						association with the SAI Trading Company in Europe.</p>
+		                <p><a href="" alt="View Profile" class="btn btn-warning"><span style="color:#008">View Profile</span></a></p>
+		            </div>
+			</div>
+			<div id="vm" class="section">
+		        <div class="row" style="width:1200px; margin:0px auto">
+		            <div class="col-md-6 text-center">
+		                <h3><span style="color:#008">Our Vision</span></h3>
+		                <p>To constantly grow the value of our business throughout the East African region with the highest level of customer satisfaction.</p>
+		            </div>
+		            <div class="col-md-6 text-center">
+		                <h3><span style="color:#008">Our Mission</span></h3>
+		                <p>To provide superior construction and engineering 
+						services exceeding customer expectations through our culture of quality 
+						work, innovation, integrity, safety, teamwork and timely delivery.</p>
+		            </div>
+		        </div>
+		    </div>
+
+    		<section class="section blog-post-entry bg-light" id="section-events">
+        <div class="container">
+          <div class="row justify-content-center text-center mb-5">
+            <div class="col-md-7">
+              <h2 class="heading" data-aos="fade-up">Featured Projects</h2>
+              <p data-aos="fade-up">These are some of the projects we have worked on.</p>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-lg-4 col-md-6 col-sm-6 col-12 post" data-aos="fade-up" data-aos-delay="100">
+
+              <div class="media media-custom d-block mb-4 h-100">
+                <a href="#" class="mb-4 d-block"><img src="images/img_1.jpg" alt="Image placeholder" class="img-fluid"></a>
+                <div class="media-body">
+                  <span class="meta-post">February 26, 2018</span>
+                  <h2 class="mt-0 mb-3"><a href="#">Travel Hacks to Make Your Flight More Comfortable</a></h2>
+                  <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
+                </div>
+              </div>
+
+            </div>
+            <div class="col-lg-4 col-md-6 col-sm-6 col-12 post" data-aos="fade-up" data-aos-delay="200">
+              <div class="media media-custom d-block mb-4 h-100">
+                <a href="#" class="mb-4 d-block"><img src="images/img_2.jpg" alt="Image placeholder" class="img-fluid"></a>
+                <div class="media-body">
+                  <span class="meta-post">February 26, 2018</span>
+                  <h2 class="mt-0 mb-3"><a href="#">5 Job Types That Aallow You To Earn As You Travel The World</a></h2>
+                  <p>Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean.</p>
+                </div>
+              </div>
+            </div>
+            <div class="col-lg-4 col-md-6 col-sm-6 col-12 post" data-aos="fade-up" data-aos-delay="300">
+              <div class="media media-custom d-block mb-4 h-100">
+                <a href="#" class="mb-4 d-block"><img src="images/img_3.jpg" alt="Image placeholder" class="img-fluid"></a>
+                <div class="media-body">
+                  <span class="meta-post">February 26, 2018</span>
+                  <h2 class="mt-0 mb-3"><a href="#">30 Great Ideas On Gifts For Travelers</a></h2>
+                  <p>A small river named Duden flows by their place and supplies it with the necessary regelialia. t is a paradisematic country, in which roasted parts of sentences.</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+			<section class="instagram pt-5">
+			    <div class="container-fluid">
+			        <div class="row no-gutters justify-content-center pb-5">
+			          	<div class="col-md-7 text-center heading-section ftco-animate">
+			            	<h2><span>Instagram</span></h2>
+			          	</div>
+			        </div>
+			        <div class="row no-gutters">
+			          	<div class="col-sm-12 col-md ftco-animate">
+			            	<a href="images/insta-1.jpg" class="insta-img image-popup" style="background-image: url(images/insta-1.jpg);">
+				              	<div class="icon d-flex justify-content-center">
+				                	<span class="icon-instagram align-self-center"></span>
+				              	</div>
+				            </a>
+			          	</div>
+				        <div class="col-sm-12 col-md ftco-animate">
+				            <a href="images/insta-2.jpg" class="insta-img image-popup" style="background-image: url(images/insta-2.jpg);">
+				              	<div class="icon d-flex justify-content-center">
+				                	<span class="icon-instagram align-self-center"></span>
+				              	</div>
+				            </a>
+				        </div>
+				        <div class="col-sm-12 col-md ftco-animate">
+				            <a href="images/insta-3.jpg" class="insta-img image-popup" style="background-image: url(images/insta-3.jpg);">
+				              	<div class="icon d-flex justify-content-center">
+				                	<span class="icon-instagram align-self-center"></span>
+				              	</div>
+				            </a>
+				        </div>
+				        <div class="col-sm-12 col-md ftco-animate">
+				            <a href="images/insta-4.jpg" class="insta-img image-popup" style="background-image: url(images/insta-4.jpg);">
+				              	<div class="icon d-flex justify-content-center">
+				                	<span class="icon-instagram align-self-center"></span>
+				              	</div>
+				            </a>
+				        </div>
+				        <div class="col-sm-12 col-md ftco-animate">
+				            <a href="images/insta-5.jpg" class="insta-img image-popup" style="background-image: url(images/insta-5.jpg);">
+				              	<div class="icon d-flex justify-content-center">
+				                	<span class="icon-instagram align-self-center"></span>
+				              	</div>
+				            </a>
+				        </div>
+        			</div>
+      			</div>
+    		</section>
+
+
+
+
+	      	<section class="section contact-section" id="section-contact">
+		      	<div class="container">
+			        <div class="row no-gutters justify-content-center pb-5">
+			          	<div class="col-md-7 text-center heading-section ftco-animate">
+			            	<h2><span>Contact Us</span></h2>
+			          	</div>
+			        </div>
+		        	<div class="row">
+		          		<div class="col-md-7">
+		            
+		            		<form method="post" class="bg-white p-md-5 p-4 mb-5 border">
+		              			<div class="row">
+		                			<div class="col-md-6 form-group">
+		                  				<label for="name">Name</label>
+		                  				<input type="text" name="name" id="name" class="form-control ">
+		                			</div>
+		                			<div class="col-md-6 form-group">
+				                  		<label for="phone">Phone</label>
+				                  		<input type="text" name="phone" id="phone" class="form-control ">
+		                			</div>
+		              			</div>
+		          
+					            <div class="row">
+					                <div class="col-md-12 form-group">
+					                  	<label for="email">Email</label>
+					                  	<input type="email" name="email" id="email" class="form-control ">
+					                </div>
+					            </div>
+					            <div class="row mb-4">
+					                <div class="col-md-12 form-group">
+					                  	<label for="message">Write Message</label>
+					                  	<textarea name="message" name="message" id="message" class="form-control " cols="30" rows="8"></textarea>
+					                </div>
+					            </div>
+			              		<div class="row">
+			                		<div class="col-md-6 form-group">
+			                  			<input type="submit" value="Send Message" class="btn btn-primary text-white font-weight-bold">
+			                  			<div class="submitting"></div>
+			                		</div>
+			              		</div>             
+			            	</form>         
+			          	</div>
+			          	<div class="col-md-5">
+			            	<div class="row">
+			              		<div class="col-md-10 ml-auto contact-info">
+			                		<p><span class="d-block">Address:</span> <span class="text-black"> Aqua House(3rd floor)<br> Plot 321 Ggaba Road <br> P.O.Box 1615 <br> Kansanga(Next Kampala International University)</span></p>
+			                		<p><span class="d-block">Phone:</span> <span class="text-black"> (+256) 774 892638 <br> (+256) 706799511 <br> (+256) 759791283</span></p>
+			                		<p><span class="d-block">Email:</span> <span class="text-black"> dewibuildings</DEL>@gmail.com</span></p>
+			              		</div>
+			            	</div>
+			          	</div>
+			        </div>
+			    </div>
+		    </section>
+
+		    <?php include("footer.html"); ?>
+
+		<!--=================scripts==========================-->
+		<script src="js/jquery-3.3.1.min.js"></script>
+		<script src="styles/bootstrap-4.1.2/popper.js"></script>
+		<script src="styles/bootstrap-4.1.2/bootstrap.min.js"></script>
+		<script src="plugins/greensock/TweenMax.min.js"></script>
+		<script src="plugins/greensock/TimelineMax.min.js"></script>
+		<script src="plugins/scrollmagic/ScrollMagic.min.js"></script>
+		<script src="plugins/greensock/animation.gsap.min.js"></script>
+		<script src="plugins/greensock/ScrollToPlugin.min.js"></script>
+		<script src="plugins/OwlCarousel2-2.3.4/owl.carousel.js"></script>
+		<script src="plugins/easing/easing.js"></script>
+		<script src="plugins/progressbar/progressbar.min.js"></script>
+		<script src="plugins/parallax-js-master/parallax.min.js"></script>
+		<script src="plugins/jquery-datepicker/jquery-ui.js"></script>
+		<script src="plugins/colorbox/jquery.colorbox-min.js"></script>
+		<script src="js/custom.js"></script>
+
+		<script src="js/jquery.min.js"></script>
+  		<script src="js/jquery-migrate-3.0.1.min.js"></script>
+  		<script src="js/popper.min.js"></script>
+		<script src="js/bootstrap.min.js"></script>
+		<script src="js/jquery.easing.1.3.js"></script>
+		<script src="js/jquery.waypoints.min.js"></script>
+		<script src="js/jquery.stellar.min.js"></script>
+		<script src="js/owl.carousel.min.js"></script>
+		<script src="js/jquery.magnific-popup.min.js"></script>
+		<script src="js/aos.js"></script>
+		<script src="js/jquery.animateNumber.min.js"></script>
+		<script src="js/bootstrap-datepicker.js"></script>
+		<script src="js/jquery.timepicker.min.js"></script>
+		<script src="js/scrollax.min.js"></script>
+		<script src="js/main.js"></script>
+		<!--===scripts====-->
+	</body>
+</html>
