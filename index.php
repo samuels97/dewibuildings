25600eac379200cabec56033423e1d7e3bc8271a
samuels97 <66149372+samuels97@users.noreply.github.com>
2020-08-14 15:30:18 +0000

    first change -connecting to bootstrap online

--- index.php
+++ index.php
@@ -7,7 +7,8 @@
 		<meta name="description" content="The River template project">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 
-		<link rel="stylesheet" type="text/css" href="styles/bootstrap-4.1.2/bootstrap.min.css">
+		<!--<link rel="stylesheet" type="text/css" href="styles/bootstrap-4.1.2/bootstrap.min.css">-->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.2/css/bootstrap.min.css">
 		<link href="plugins/font-awesome-4.7.0/css/font-awesome.min.css" rel="stylesheet" type="text/css">
 		<link rel="stylesheet" type="text/css" href="plugins/OwlCarousel2-2.3.4/owl.carousel.css">
 		<link rel="stylesheet" type="text/css" href="plugins/OwlCarousel2-2.3.4/owl.theme.default.css">
